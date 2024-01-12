8a669ebe79e3aa5450c28edf7a1c5b9061118771
imbesky <123881839+imbesky@users.noreply.github.com>
2024-01-12 22:31:30 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    for(int i = 0;i < n;i++){
+        int a,b;
+        cin >> a;
+        cin >> b;
+        cout << "Case #" << i + 1 << ": " << a + b << "\n";
+    }
+    
+    return 0;
+}
